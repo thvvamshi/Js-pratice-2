e193b480f0e36e2ab4fc49ab205f0d1c73106360
thvvamshi <bodavamshikumar2002@gmail.com>
2025-01-12 13:59:02 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 window.addEventListener("mousemove",(dets)=>{
     let card = document.getElementById("card");
+    let silde1 = gsap.utils.mapRange(0,window.innerHeight,100+card.getBoundingClientRect().height/2,window.innerHeight-100,dets.clientY)
     let silde = gsap.utils.mapRange(0,window.innerWidth,100+card.getBoundingClientRect().width/2,window.innerWidth-100,dets.clientX)
+    gsap.to( card ,{
+        top: silde1+'px',
+        ease:Power4,
+    });
     gsap.to( card ,{
         left: silde+'px',
         ease:Power4,
